94e2448d2370664aa4266d919d4ef47b70cdcb7e
Nick-800 <sohaibkmsh@gmai.com>
2024-08-14 12:24:02 +0200

    lol

--- app/Http/Controllers/ServerController.php
+++ app/Http/Controllers/ServerController.php
@@ -14,13 +14,13 @@ class ServerController extends Controller
         $inputs = $request->validate([
             'name' => ['required', 'string'],
         ]);
-        if (Server::where('user_id', '=', auth()->id())->exists()) {
+        if (Server::where('user_id', '=', auth("api")->id())->exists()) {
             return response()->json([
                 'data' => 'you can not create more then one server'
             ], 422);
         }
         $code = $this->generateCode();
-        auth()->user()->servers()->create([
+        auth("api")->user()->servers()->create([
             'code' => $code,
             'name' => $inputs['name']
         ]);
@@ -82,26 +82,5 @@ class ServerController extends Controller
             $this->generateCode();
         }
         return $code;
-    }
-    public function index(){
-        $servers=Server::where('user_id','=',auth()->id())->get();
-        if($servers==null){
-            return response()->json(['data'=>'the user doesn`t have any servers']) ;
-        }
-        return response()->json(['data'=>$servers]);
-    }
-    public function show($id){
-        if(!Server::where('id','=', $id)->exists()){
-            return response()->json(['data'=>' no such servers']);
-        }
-        $server=Server::where('user_id','=',auth()->id());
-        if($server==null){
-            return response()->json(['data'=>'user has no servers']);
-        }
-        $server=$server->find($id);
-        if($server==null){
-            return response()->json(['data'=> 'user not allowed']);
-        }
-        return response()->json(['data'=>$server]);
     }
 }
